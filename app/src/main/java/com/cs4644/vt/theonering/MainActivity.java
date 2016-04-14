16ea47f148ba20e3d5f45a367254778615aaa084
Kelvin Aviles <kaviles@vt.edu>
2016-04-14 14:26:15 -0400

    Test commit to understand Android Studio Git UI.

--- app/src/main/java/com/cs4644/vt/theonering/MainActivity.java
+++ app/src/main/java/com/cs4644/vt/theonering/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    // This is a test comment please disregard.
+
     MusicManager mm;
     TextManager tm;
     WeatherManager wm;
