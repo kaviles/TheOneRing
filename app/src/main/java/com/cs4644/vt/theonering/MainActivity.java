fb00425f590be6e72fba98b23bf68812fcb00f87
Nicholas Hu <nick363@vt.edu>
2016-04-19 08:21:17 -0400

    Adding Music Functionality and 3 song files

--- app/src/main/java/com/cs4644/vt/theonering/MainActivity.java
+++ app/src/main/java/com/cs4644/vt/theonering/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -12,16 +13,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MusicManager mm;
     TextManager tm;
     WeatherManager wm;
+    MediaPlayer[] mediaPlayer;
+    int songCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mm = new MusicManager(this);
         tm = new TextManager(this);
         wm = new WeatherManager(this);
-
+        mediaPlayer = new MediaPlayer[3];
+        mediaPlayer[0] = MediaPlayer.create(MainActivity.this, R.raw.song1);
+        songCount = 0;
         findViewById(R.id.btn_music_back).setOnClickListener(this);
         findViewById(R.id.btn_music_play).setOnClickListener(this);
         findViewById(R.id.btn_music_pause).setOnClickListener(this);
@@ -38,18 +42,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (i) {
             case R.id.btn_music_back:
+                mediaPlayer[songCount].stop();
+                mediaPlayer[songCount].release();
+                mediaPlayer[songCount] = null;
+                if (songCount == 0) {
+                    songCount = 2;
+                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song3);
+                } else if (songCount == 2) {
+                    songCount--;
+                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song2);
+                } else if (songCount == 1) {
+                    songCount--;
+                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song1);
+                }
+                mediaPlayer[songCount].start();
                 mm.skipBackMusic();
-
                 break;
             case R.id.btn_music_play:
                 mm.playMusic();
-
+                mediaPlayer[songCount].start();
                 break;
             case R.id.btn_music_pause:
+                mediaPlayer[songCount].pause();
                 mm.pauseMusic();
                 break;
             case R.id.btn_music_forward:
                 mm.skipForwardMusic();
+                mediaPlayer[songCount].stop();
+                mediaPlayer[songCount].release();
+                mediaPlayer[songCount] = null;
+                //this.onDestroy();
+                if (songCount == 0) {
+                    songCount++;
+                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song2);
+                } else if (songCount == 1) {
+                    songCount++;
+                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song3);
+                } else {
+                    songCount = 0;
+                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song1);
+                }
+                mediaPlayer[songCount].start();
                 break;
             case R.id.btn_read_texts:
                 tm.readTexts();
