b9ccd6f62679b0f6965c403b4719d3a72960d6e8
Kelvin Aviles <kaviles@vt.edu>
2016-04-26 19:20:45 -0400

    Change button layout to four buttons. Moved music functions from MainActivity to MusicManger class.

--- app/src/main/java/com/cs4644/vt/theonering/MainActivity.java
+++ app/src/main/java/com/cs4644/vt/theonering/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     MusicManager mm;
     TextManager tm;
     WeatherManager wm;
-    MediaPlayer[] mediaPlayer;
-    int songCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +21,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mm = new MusicManager(this);
         tm = new TextManager(this);
         wm = new WeatherManager(this);
-        mediaPlayer = new MediaPlayer[3];
-        mediaPlayer[0] = MediaPlayer.create(MainActivity.this, R.raw.song1);
-        songCount = 0;
-        findViewById(R.id.btn_music_back).setOnClickListener(this);
-        findViewById(R.id.btn_music_play).setOnClickListener(this);
-        findViewById(R.id.btn_music_pause).setOnClickListener(this);
-        findViewById(R.id.btn_music_forward).setOnClickListener(this);
-        findViewById(R.id.btn_read_texts).setOnClickListener(this);
-        findViewById(R.id.btn_read_weather).setOnClickListener(this);
 
+        findViewById(R.id.btn_up).setOnClickListener(this);
+        findViewById(R.id.btn_left).setOnClickListener(this);
+        findViewById(R.id.btn_right).setOnClickListener(this);
+        findViewById(R.id.btn_down).setOnClickListener(this);
     }
 
     @Override
@@ -41,54 +34,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int i = v.getId();
 
         switch (i) {
-            case R.id.btn_music_back:
-                mediaPlayer[songCount].stop();
-                mediaPlayer[songCount].release();
-                mediaPlayer[songCount] = null;
-                if (songCount == 0) {
-                    songCount = 2;
-                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song3);
-                } else if (songCount == 2) {
-                    songCount--;
-                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song2);
-                } else if (songCount == 1) {
-                    songCount--;
-                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song1);
-                }
-                mediaPlayer[songCount].start();
+            case R.id.btn_left:
                 mm.skipBackMusic();
                 break;
-            case R.id.btn_music_play:
+            case R.id.btn_up:
                 mm.playMusic();
-                mediaPlayer[songCount].start();
                 break;
-            case R.id.btn_music_pause:
-                mediaPlayer[songCount].pause();
+            case R.id.btn_down:
                 mm.pauseMusic();
                 break;
-            case R.id.btn_music_forward:
+            case R.id.btn_right:
                 mm.skipForwardMusic();
-                mediaPlayer[songCount].stop();
-                mediaPlayer[songCount].release();
-                mediaPlayer[songCount] = null;
-                //this.onDestroy();
-                if (songCount == 0) {
-                    songCount++;
-                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song2);
-                } else if (songCount == 1) {
-                    songCount++;
-                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song3);
-                } else {
-                    songCount = 0;
-                    mediaPlayer[songCount] = MediaPlayer.create(MainActivity.this, R.raw.song1);
-                }
-                mediaPlayer[songCount].start();
-                break;
-            case R.id.btn_read_texts:
-                tm.readTexts();
-                break;
-            case R.id.btn_read_weather:
-                wm.readWeather();
                 break;
             default:
                 break;
